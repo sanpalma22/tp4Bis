cd06c67141464e3c69b5b38d914fe531ff0bbb78
sanpalma22 <spama0610@gmail.com>
2023-05-31 01:24:55 -0300

    Terminado

--- Models/Pais.cs
+++ Models/Pais.cs
@@ -3,14 +3,14 @@ public class Pais{
     public string Nombre{get;private set;}
     public string imgBandera{get;private set;}
     public int Poblacion{get;private set;}
-    public DateTime FechaIndependencia{get;private set;}
+    public DateOnly FechaHistorica{get;private set;}
     public string AtractivosTuristicos{get;private set;}
 
-    public Pais(string nom, string img, int pob, DateTime FI, string AT){
+    public Pais(string nom, string img, int pob, DateOnly FP, string AT){
         Nombre=nom;
         imgBandera=img;
         Poblacion=pob;
-        FechaIndependencia=FI;
+        FechaHistorica=FP;
         AtractivosTuristicos=AT;
     }
 }
